4e8bfec8bba8001f888d2ae196d92fd33a013897
Yarden Golan <yardengolan@Yardens-MacBook-Air.local>
2017-11-15 04:13:44 +0200

    First commit

---
+++ server.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs  = require('fs');
+
+var app = express();
+
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+
+  var log = now + ': ' + req.method + ' ---  ' + req.url;
+  console.log(log);
+
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if(err){
+      console.log('Unable to append to server.log');
+    }
+  });
+  next();
+});
+
+app.use((req, res, next) => {
+  var maintenanceFlag = false;
+  if(maintenanceFlag)
+  {
+      res.render('maintenance.hbs', {
+        pageTitle: 'maintenance page',
+      });
+  }else{
+    next();
+  }
+});
+
+
+app.use(express.static(__dirname + '/public'));
+
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
+
+app.get('/', (req, res) => {
+  // res.send('<h1>Hello!!</h1>');
+    res.render('home.hbs', {
+      pageTitle: 'Home page',
+      welcomeMsg: 'Welcome Msg!!!!!'
+    });
+
+});
+
+app.get('/about',(req, res) => {
+    res.render('about.hbs', {
+      pageTitle: 'About page',
+    });
+});
+
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMsg: 'Unable to handle request.'
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server is up on port 3000');
+});
